73c917ec6a2a07109b66eb153b0fcc43ccbe5ab7
BoyanDimov20 <boyandimov20@gmail.com>
2023-04-19 17:28:44 +0300

    Add ChampionMasteryEndpoint

---
+++ src/OpenRiotAPI/OpenRiotAPI/Endpoints/ChampionMasteryEndpoint/Dtos/ChampionMastery.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenRiotAPI.Endpoints.ChampionMasteryEndpoint.Dtos;
+
+public class ChampionMastery
+{
+    public long ChampionPointsUntilNextLevel { get; set; }
+    public bool ChestGranted { get; set; }
+    public long ChampionId { get; set; }
+    public long LastPlayTime { get; set; }
+    public int ChampionLevel { get; set; }
+    public string SummonerId { get; set; }
+    public int ChampionPoints { get; set; }
+    public long ChampionPointsSinceLastLevel { get; set; }
+    public int TokensEarned { get; set; }
+}
---
+++ src/OpenRiotAPI/OpenRiotAPI/Endpoints/ChampionMasteryEndpoint/IChampionMasteryEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using OpenRiotAPI.Common;
+using OpenRiotAPI.Endpoints.ChampionMasteryEndpoint.Dtos;
+
+namespace OpenRiotAPI.Endpoints.ChampionMasteryEndpoint;
+
+public interface IChampionMasteryEndpoint
+{
+    Task<RiotResponse<IEnumerable<ChampionMastery>>> GetAllBySummonerId(Region region, string summonerId);
+    Task<RiotResponse<ChampionMastery>> GetChampionBySummonerId(Region region, string summonerId, string championId);
+    Task<RiotResponse<IEnumerable<ChampionMastery>>> GetHighestMasteryBySummonerId(Region region, string summonerId);
+    Task<RiotResponse<int>> GetSummonerMasteryBySummonerId(Region region, string summonerId);
+    
+    Task<RiotResponse<IEnumerable<ChampionMastery>>> GetAllBySummonerName(Region region, string summonerName);
+    Task<RiotResponse<ChampionMastery>> GetChampionBySummonerName(Region region, string summonerName, string championId);
+    Task<RiotResponse<IEnumerable<ChampionMastery>>> GetHighestMasteryBySummonerName(Region region, string summonerName);
+    Task<RiotResponse<int>> GetSummonerMasteryBySummonerName(Region region, string summonerName);
+
+}
